719121240eeeed49fe0a266aeebb5a5ef053a521
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-03-03 19:47:27 +0545

    added catch to all promises

--- client/src/api/notes.ts
+++ client/src/api/notes.ts
@@ -5,57 +5,57 @@ import { getPublicId } from './publicIP';
 // const endPoint = 'http://localhost/api'; //  development endpoint
 const endPoint = '/api'; //  production endpoint
 export async function addNote(Note: NoteInterface): Promise<Response> {
-  return fetch(`${endPoint}/addNote`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ...Note,
-      publicId: await getPublicId().then((res) => res.ip),
-    }),
-  }).then((res: Response) => res.json());
+	return await fetch(`${endPoint}/addNote`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			...Note,
+			publicId: await getPublicId().then((res) => res.ip),
+		}),
+	}).then((res: Response) => res.json());
 }
 
-export function getAllNotes(): Promise<NoteInterface[]> {
-  return fetch(`${endPoint}/getNotes`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((res: Response) => res.json());
+export async function getAllNotes(): Promise<NoteInterface[]> {
+	return await fetch(`${endPoint}/getNotes`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then((res: Response) => res.json());
 }
 
-export function getNote(noteId: number): Promise<NoteInterface> {
-  return fetch(`${endPoint}/getNote/${noteId}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((res: Response) => res.json());
+export async function getNote(noteId: number): Promise<NoteInterface> {
+	return await fetch(`${endPoint}/getNote/${noteId}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then((res: Response) => res.json());
 }
 
 export async function updateNote(
-  noteId: number,
-  newNote: NoteInterface
+	noteId: number,
+	newNote: NoteInterface
 ): Promise<Response> {
-  return fetch(`${endPoint}/updateNote/${noteId}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ...newNote,
-      publicId: await getPublicId().then((res) => res.ip),
-    }),
-  }).then((res: Response) => res.json());
+	return await fetch(`${endPoint}/updateNote/${noteId}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			...newNote,
+			publicId: await getPublicId().then((res) => res.ip),
+		}),
+	}).then((res: Response) => res.json());
 }
 
-export function deleteNote(noteId: number): Promise<Response> {
-  return fetch(`${endPoint}/deleteNote/${noteId}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((res: Response) => res.json());
+export async function deleteNote(noteId: number): Promise<Response> {
+	return await fetch(`${endPoint}/deleteNote/${noteId}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then((res: Response) => res.json());
 }
--- client/src/api/publicIP.ts
+++ client/src/api/publicIP.ts
@@ -1,11 +1,13 @@
 import PublicIP from '../models/PublicIP';
 
-export function getPublicId(): Promise<PublicIP> {
-	return fetch('https://api.ipify.org/?format=json', {
+export async function getPublicId(): Promise<PublicIP> {
+	return await fetch('https://api.ipify.org/?format=json', {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
 			'Access-Control-Allow-Origin': '*',
 		},
-	}).then((res: Response) => res.json());
+	})
+		.then((res: Response) => res.json())
+		.catch((error) => error);
 }
